c89e74479005338fc41afba3451e96f4983ba756
Benny <bennybc1@hotmail.com>
2025-01-03 22:44:19 -0500

    solve prime_number_check with some weird returning number, jsjsj, actually I have a story to tell for that number but maybe other time.

---
+++ Easy - Problems/5_prime_number_check.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // get number
+        // check if it's divisible for other number than himself and one
+
+    int n = 10171111; // 13 is also prime and is the sum jsjsjs
+
+    for (int i = 3; i < n; ++i)
+    {
+        if(n%i == 0){
+            cout << "Not prime because it can be divided by: " << i << endl;
+            return (double)-1.45;
+        }
+    }
+    cout << "Prime" << endl;
+    return 0;
+}
